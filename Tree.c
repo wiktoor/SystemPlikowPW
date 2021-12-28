0bc5a422fba331cd3e49631738f249c09dda7b9a
Wiktor Chmielewski <wc429131@students.mimuw.edu.pl>
2021-12-28 09:38:12 +0100

    Poprawiono funkcję tree_free

--- Tree.c
+++ Tree.c
@@ -18,8 +18,13 @@ Tree* tree_new() {
 
 void tree_free(Tree *tree) {
     pthread_mutex_destroy(&tree->mutex);
+    const char* key = NULL;
+    void* value = NULL;
+    HashMapIterator it = hmap_iterator(tree->map);
+    while (hmap_next(tree->map, &it, &key, &value)) {
+        tree_free(value);
+    }
     hmap_free(tree->map);
     free(tree->address);
-    free(tree->parent); // ???
     free(tree);
 }
